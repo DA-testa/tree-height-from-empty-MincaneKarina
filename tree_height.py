b9c98d5f3153f70c94e0db2ec8479fd36bd40277
MincaneKarina <karina.mincane@edu.rtu.lv>
2023-03-05 10:07:58 +0200

    1

--- tree_height.py
+++ tree_height.py
@@ -8,12 +8,82 @@ def compute_height(n, parents):
     # Write this function
     max_height = 0
     # Your code here
+
+    list = []
+    for p_str in parents.split():
+        p_int = int(p_str)
+        list.append(p_int)
+    #print(list)
+
+    i = 0
+    x = 0
+    k = 0
+    a =[]
+    for index in range(0, n):
+        a.insert(index,0)
+    b = [None] * n
+    c = []
+    while x != n:
+        #print(list[i])
+        
+        if b[i] != 1:
+            c.append(i)
+            #print(c)
+            for item in c:
+                a[item] = a[item] + 1
+                
+        else: 
+            #print(i)
+            if c:
+                a[c[-1]] =a[i]  +1
+            else: a[list.index(i)] =a[i]  +1
+            c =[]   
+        #print(a)
+        b[i] = 1
+        #a[i] = 0
+        k = k + 1
+        
+        i = list[i]
+
+        if i == -1:
+            
+            x = 0
+            j = list.index(i)
+
+            k=0
+            c = []
+            for item in b:
+                if item == None:
+                    i = b.index(item)  
+                    break
+                else: x = x + 1
+
+    #print("Apmeklejums")
+    #print(b)
+    #print("a")
+    #print(a)
+
+    max_height = 0
+    for item in a:
+        if item>max_height:
+            max_height = item
+
     return max_height
 
 
 def main():
     # implement input form keyboard and from files
-    
+    text = input()
+    if "I" in text:
+        n = int(input())
+        parents = input()
+        print(compute_height(n,parents))
+    elif "F" in text:
+        filename = input()
+        with open ("./test/" + filename, mode="r") as file:
+            n = file.readline()
+            parents =file.readline()
+        print(compute_height(n,parents))
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
     
